8b5fdbdfbb967159912ae6d8f614736fc2f13289
Juan Pablo <juanp.klix@mi.unc.edu.ar>
2026-08-18 01:43:03 -0300

    Add necesary dependences for plugin matrix

--- lua/plugins/matrix.lua
+++ lua/plugins/matrix.lua
@@ -1,22 +1,22 @@
-  return{
-    {
-    "matrix-tema",
-    dir = "~/plugins-manuales/matrix-nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("matrix")
-    end,
-  },
-  {
-    "matrix-lluvia",
-    dir = "/home/nazojox/plugins-manuales/vimatrix.nvim",
-    lazy = false,
-    
-    config = function()
-      require("vimatrix").setup({})
-      vim.api.nvim_create_user_command("Matrix", "VimatrixOpen", {})
-      vim.api.nvim_create_user_command("MatrixO", "VimatrixClose", {})
-    end,
-  },
-  }
+return {
+	{
+		"matrix-tema",
+		dir = vim.fn.stdpath("config") .. "/plugins-manuales/matrix-nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("matrix")
+		end,
+	},
+	{
+		"matrix-lluvia",
+		dir = vim.fn.stdpath("config") .. "/plugins-manuales/vimatrix.nvim",
+		lazy = false,
+
+		config = function()
+			require("vimatrix").setup({})
+			vim.api.nvim_create_user_command("Matrix", "VimatrixOpen", {})
+			vim.api.nvim_create_user_command("MatrixO", "VimatrixClose", {})
+		end,
+	},
+}
